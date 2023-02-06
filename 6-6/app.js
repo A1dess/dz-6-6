127ac2938f76a319feb1a9b76deebb65c30fbcbc
A1dess <116088054+A1dess@users.noreply.github.com>
2023-02-06 23:20:42 +0600

    Add files via upload

---
+++ 6-6/app.js
@@ -0,0 +1,27 @@
+document.querySelector("#submit").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+  function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+  
+  document.querySelector("#key1").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+  function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+  
+  document.querySelector("#key2").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+  function alerted(){
+    alert("Вы нажали на кнопку");
+  }
+  
+  document.querySelector("#key3").onclick = function(){
+    alert("Вы нажали на кнопку");
+  }
+  function alerted(){
+    alert("Вы нажали на кнопку");
+  }
